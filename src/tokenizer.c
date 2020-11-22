7a0bfd399e46f253e1f5e64d32c597d3266fdfac
student <laberroutra@miners.utep.edu>
2020-11-22 02:38:54 -0700

    Fixing pointers and allocate memory for the string, fix file tokenizer.c and added space_char() function

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -2,6 +2,9 @@
 
 int space_char(char c)
 {
-  
+  if((c == '\t') || (c ==' ')){
+      return 1;
+    }
+    return 0;
 }
 
--- src/uimain.c
+++ src/uimain.c
@@ -11,9 +11,8 @@ int main()
   puts("Welcome!");
   int c = 0;
   char s[30] = "Hello World!";             //Limit of 19 characters, HARDCORED FOR TESTS
-  char *sptr = malloc((strlen(s)+1) * sizeof(char));  //THEN WE NEED TO COPY THE STRING
-  strcpy(sptr,s);  // allocate string
-  
+  char *sptr = (char *) malloc(31);  //THEN WE NEED TO COPY THE STRING
+  sptr = &s[0];
 
   while (1){                               //Infinite while loop
     printf("\nSelect one the next options: (a,b, or c)\n");
@@ -30,14 +29,18 @@ int main()
     switch(c){                             // switch for user options
     case 'a':
       printf("  Write your new line\n  > ");
-      //strcpy("Hello World!",s);
+      
       
       //fgets(s, sizeof(s), stdin);          // read string
 
       /* Use the functions from the tokenizer */
 
       
-      printf("Your string is: %c",sptr[0]);
+      printf("Your string is: %c\n",sptr[1]);
+      for (int i=0; i<sizeof(s);i++){
+	printf("Is char %c whitespace? %d\n", sptr[i], space_char(sptr[i]));
+      }
+      
       //puts(s);                             // displays string
       //printf("%s\n",s);                      //TEST: PRINT HARDCORED STRING
 
